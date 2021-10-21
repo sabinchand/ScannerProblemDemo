f93555f528684ddc5c711472183433121855f85c
Sabin Chand <80932595+sabinchand@users.noreply.github.com>
2021-10-22 01:46:21 +0545

    Scanner Problem Demo

--- ScannerProblem/src/ScannerProblemDemo.java
+++ ScannerProblem/src/ScannerProblemDemo.java
@@ -1,8 +1,8 @@
-	//Sometimes we face problems when we are trying to input "String" after inputting int/float/double/long/byte/short/char
-	//It is because before coming to the String we've hit the enter button and the String takes that enter as "\n" String characters.
-	//Hence by hitting enter, we have already given the input to the String.
-	//And it goes to the next statement.
-	//The problem and two solutions are shown below in separate methods below.
+//Sometimes we face problems when we are trying to input "String" after inputting int/float/double/long/byte/short/char
+//It is because before coming to the String we've hit the enter button and the String takes that enter as "\n" String characters.
+//Hence by hitting enter, we have already given the input to the String.
+//And it goes to the next statement.
+//The problem and two solutions are shown below in separate methods below.
 	
 import java.util.Scanner;
 public class ScannerProblemDemo{
